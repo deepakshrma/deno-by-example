3485f88e72eb8de5f103d5ad0399c3dcfa2fdbc0
xdeepakv <deepak.m.shrma@gmail.com>
2020-05-29 02:50:06 +0800

    added css and clean theme

---
+++ config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  plugins: [
+    "@docusaurus/plugin-google-analytics",
+    "@docusaurus/plugin-sitemap",
+  ],
+  stylesheets: [
+    {
+      href:
+        "https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&family=Open+Sans&display=swap",
+      type: "text/css",
+    },
+  ],
+  themeConfig: {
+    sidebarCollapsible: false,
+    prism: {
+      defaultLanguage: "ts",
+    },
+    googleAnalytics: {
+      trackingID: "",
+    },
+    footer: {
+      links: [
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Medium.com",
+              href: "https://medium.com/@deepak_v",
+            },
+            {
+              label: "Discord",
+              href: "https://discord.com/channels/@deepakv",
+            },
+            {
+              label: "Linkdin",
+              href: "https://www.linkedin.com/in/xdeepakv/",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/deepakshrma/deepakshrma.github.io",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Deepak Vishwakarma, Inc. Built with Docusaurus.`,
+    },
+  },
+  themes: ["@docusaurus/theme-classic", "@docusaurus/theme-live-codeblock"],
+};
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,4 @@
+const configs = require("./config");
 module.exports = {
   title: "Deno By Example",
   tagline: "Deno By Example",
@@ -6,27 +7,15 @@ module.exports = {
   favicon: "img/favicon.ico",
   organizationName: "deepakshrma", // Usually your GitHub org/user name.
   projectName: "deno-by-example", // Usually your repo name.
-  plugins: ['@docusaurus/plugin-google-analytics', '@docusaurus/plugin-sitemap'],
+  ...configs,
   themeConfig: {
+    ...configs.themeConfig,
     sidebarCollapsible: false,
-    prism: {
-      // theme: require("prism-react-renderer/themes/github"),
-      // darkTheme: require("prism-react-renderer/themes/dracula"),
-      defaultLanguage: "ts",
-    },
-    // announcementBar: {
-    //   id: 'support_us', // Any value that will identify this message.
-    //   content:
-    //     'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
-    //   backgroundColor: '#fafbfc', // Defaults to `#fff`.
-    //   textColor: '#091E42', // Defaults to `#000`.
-    // },
     googleAnalytics: {
       trackingID: "UA-69472059-2",
     },
     navbar: {
       hideOnScroll: true,
-
       title: "Deno By Example",
       logo: {
         alt: "Deno By Exampl",
@@ -39,7 +28,6 @@ module.exports = {
           label: "Docs",
           position: "left",
         },
-        // { to: "blog", label: "Blog", position: "left" },
         {
           href: "https://github.com/deepakshrma/deno-by-example",
           label: "GitHub",
@@ -47,75 +35,25 @@ module.exports = {
         },
       ],
     },
-    // footer: {
-    //   style: 'dark',
-    //   links: [
-    //     {
-    //       title: 'Docs',
-    //       items: [
-    //         {
-    //           label: 'Style Guide',
-    //           to: 'docs/doc1',
-    //         },
-    //         {
-    //           label: 'Second Doc',
-    //           to: 'docs/doc2',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'Community',
-    //       items: [
-    //         {
-    //           label: 'Stack Overflow',
-    //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-    //         },
-    //         {
-    //           label: 'Discord',
-    //           href: 'https://discordapp.com/invite/docusaurus',
-    //         },
-    //         {
-    //           label: 'Twitter',
-    //           href: 'https://twitter.com/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: 'More',
-    //       items: [
-    //         {
-    //           label: 'Blog',
-    //           to: 'blog',
-    //         },
-    //         {
-    //           label: 'GitHub',
-    //           href: 'https://github.com/facebook/docusaurus',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    // },
   },
-  themes: ["@docusaurus/theme-classic", "@docusaurus/theme-live-codeblock"],
   presets: [
     [
       "@docusaurus/preset-classic",
       {
         docs: {
           routeBasePath: "/", // Set this value to '/'.
-          // homePageId: '01_hello_world', // Set to existing document id.
           path: "website",
-          // routeBasePath: 'website',
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
           editUrl: "https://github.com/deepakshrma/deno-by-example/edit/master",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           editUrl:
             "https://github.com/deepakshrma/deno-by-example/edit/master/blog",
+          feedOptions: {
+            type: "all",
+            copyright: `Copyright © ${new Date().getFullYear()} Deepak Vishwakarma, Inc. Built with Docusaurus.`,
+          },
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
